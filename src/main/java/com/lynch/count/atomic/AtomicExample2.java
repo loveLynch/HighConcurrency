d5fdd0a299684012d2d076759ebacad0babffdff
15715198163 <lovelynch@163.com>
2019-11-28 22:25:26 +0800

    first commit

---
+++ src/main/java/com/lynch/count/atomic/AtomicExample2.java
@@ -0,0 +1,55 @@
+package com.lynch.count.atomic;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by lynch on 2019-11-28. <br>
+ **/
+@Slf4j
+//线程安全
+// AtomicLong
+//-----unsafe类
+//-----CAS(compareAndSwapLong)
+//--------native
+public class AtomicExample2 {
+
+    //请求总数
+    public static int clientTotal = 5000;
+    //同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    public static AtomicLong count = new AtomicLong(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}", count.get());
+    }
+
+    private static void add() {
+        count.incrementAndGet(); //先增加再取值
+//        count.getAndIncrement();//先取值再增加
+    }
+}
---
+++ src/main/java/com/lynch/count/atomic/AtomicExample3.java
@@ -0,0 +1,55 @@
+package com.lynch.count.atomic;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * Created by lynch on 2019-11-28. <br>
+ **/
+@Slf4j
+//线程安全
+// LongAdder
+//LongAdder与AtomicLong
+//AtomicLong 线程低时，全局唯一订单（id)适用
+//LongAdder  并行更高时使用，但统计的时候，可能出现误差
+public class AtomicExample3 {
+
+    //请求总数
+    public static int clientTotal = 5000;
+    //同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    public static LongAdder count = new LongAdder();
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}", count);
+    }
+
+    private static void add() {
+        count.increment();
+    }
+}
